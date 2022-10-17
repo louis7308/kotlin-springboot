b202e50771422671a28f3f2c3e8317cbcd379260
louis7308 <jswa7308@naver.com>
2022-10-17 12:08:57 +0900

    :tada: :: project start

---
+++ crud/src/main/kotlin/com/example/crud/CrudApplication.kt
@@ -0,0 +1,11 @@
+package com.example.crud
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CrudApplication
+
+fun main(args: Array<String>) {
+    runApplication<CrudApplication>(*args)
+}
