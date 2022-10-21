712afa50d773be96bcdd3b58de77a0b6b2126886
louis7308 <jswa7308@naver.com>
2022-10-21 10:26:30 +0900

    :wrench: junit test implementation

--- crud/build.gradle.kts
+++ crud/build.gradle.kts
@@ -40,6 +40,9 @@ dependencies {
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.kotest:kotest-runner-junit5:5.5.1")
+    testImplementation("io.kotest:kotest-assertions-core:5.5.1")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.1")
 }
 
 tasks.withType<KotlinCompile> {
