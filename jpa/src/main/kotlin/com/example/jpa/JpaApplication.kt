38a2baecf9f45e4b5ce17d656345fe560f5b6921
louis7308 <jswa7308@naver.com>
2022-10-22 14:38:49 +0900

    :tada: :: JPA study project

---
+++ jpa/src/main/kotlin/com/example/jpa/JpaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.jpa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JpaApplication
+
+fun main(args: Array<String>) {
+    runApplication<JpaApplication>(*args)
+}
