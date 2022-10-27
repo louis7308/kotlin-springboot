818058d2f122207b68cdddfc79aec00865d8f6e0
louis7308 <jswa7308@naver.com>
2022-10-27 15:03:10 +0900

    :sparkles: :: Student class test code

--- jpa/src/main/kotlin/com/example/jpa/JpaApplication.kt
+++ jpa/src/main/kotlin/com/example/jpa/JpaApplication.kt
@@ -1,5 +1,7 @@
 package com.example.jpa
 
+import com.example.jpa.entity1.Student
+import com.example.jpa.entity1.Teacher
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
--- jpa/src/test/kotlin/com/example/jpa/JpaApplicationTests.kt
+++ jpa/src/test/kotlin/com/example/jpa/JpaApplicationTests.kt
@@ -1,13 +1,25 @@
 package com.example.jpa
 
+import com.example.jpa.entity1.Student
+import com.example.jpa.entity1.Teacher
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class JpaApplicationTests {
+internal class JpaApplicationTests: DescribeSpec({
+    describe("entity") {
+        context("Student 클래스가 있으면") {
+            it("해당 식에 대한 결과값이 반환") {
+                var teacher = Teacher("seung")
+                var student = Student("hi", teacher)
 
-    @Test
-    fun contextLoads() {
+                student.name shouldBe "hello"
+            }
+        }
     }
 
-}
+
+})
