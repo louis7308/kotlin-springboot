b6f42717f5e5ceead1e8ef09441f11ac1a57ebc5
louis7308 <jswa7308@naver.com>
2022-10-27 15:02:50 +0900

    :wrench: junit5 kotest

--- jpa/build.gradle.kts
+++ jpa/build.gradle.kts
@@ -36,6 +36,8 @@ dependencies {
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.kotest:kotest-runner-junit5:5.3.2")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.1")
 }
 
 tasks.withType<KotlinCompile> {
