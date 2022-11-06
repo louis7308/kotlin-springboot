425c8c91fba1bcd59520490697553237fed896c9
louis7308 <jswa7308@naver.com>
2022-11-06 15:13:38 +0900

    :sparkles: :: 투표 프로그램

---
+++ vote/src/test/kotlin/com/example/vote/VoteApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.vote
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VoteApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
