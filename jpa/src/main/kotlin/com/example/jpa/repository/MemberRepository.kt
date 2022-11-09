30ba273ede1b7b724555c32818e425cc63822494
전승원 <78971821+louis7308@users.noreply.github.com>
2022-11-09 09:44:03 +0900

    :sparkles: :: save 함수 repository

--- jpa/src/main/kotlin/com/example/jpa/repository/MemberRepository.kt
+++ jpa/src/main/kotlin/com/example/jpa/repository/MemberRepository.kt
@@ -1,4 +1,17 @@
 package com.example.jpa.repository
 
+import com.example.jpa.entity.Member
+import org.springframework.stereotype.Repository
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
+
+@Repository
 class MemberRepository {
+    @PersistenceContext
+    lateinit var entityManager: EntityManager
+
+    fun save(member: Member) : Long {
+        entityManager.persist(member)
+        return member.id
+    }
 }
