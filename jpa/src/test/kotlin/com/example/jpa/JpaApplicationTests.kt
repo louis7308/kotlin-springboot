5a705c256131fcfe7cc52921d9cb6aa02db9f9ea
전승원 <78971821+louis7308@users.noreply.github.com>
2022-11-09 09:44:41 +0900

    :fire: :: 기존 Test Code 삭제

--- jpa/src/test/kotlin/com/example/jpa/JpaApplicationTests.kt
+++ jpa/src/test/kotlin/com/example/jpa/JpaApplicationTests.kt
@@ -1,11 +1,6 @@
 package com.example.jpa
 
-import com.example.jpa.entity1.Student
-import com.example.jpa.entity1.Teacher
-import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
-import io.kotest.matchers.shouldBe
-import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
@@ -13,10 +8,7 @@ internal class JpaApplicationTests: DescribeSpec({
     describe("entity") {
         context("Student 클래스가 있으면") {
             it("해당 식에 대한 결과값이 반환") {
-                var teacher = Teacher("seung")
-                var student = Student("hi", teacher)
 
-                student.name shouldBe "hello"
             }
         }
     }
