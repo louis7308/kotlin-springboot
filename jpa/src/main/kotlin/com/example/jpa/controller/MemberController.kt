19a6172886633ab7130e61dbf9835bda6d518cd5
전승원 <78971821+louis7308@users.noreply.github.com>
2022-11-09 09:45:14 +0900

    :sparkles: :: Member save 컨트롤러

---
+++ jpa/src/main/kotlin/com/example/jpa/controller/MemberController.kt
@@ -0,0 +1,29 @@
+package com.example.jpa.controller
+
+import com.example.jpa.entity.Member
+import com.example.jpa.repository.MemberRepository
+import lombok.RequiredArgsConstructor
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.lang.NullPointerException
+import kotlin.reflect.jvm.internal.calls.ThrowingCaller.member
+
+@RestController
+@RequiredArgsConstructor
+class MemberController(
+    private val memberRepository: MemberRepository
+) {
+
+    @PostMapping("/member")
+    @Transactional
+    fun saveMember(@RequestParam("name") reqName: String) : Long {
+        val member: Member = Member(
+            id = 1L,
+            username = reqName
+        )
+        return memberRepository.save(member)
+    }
+}
