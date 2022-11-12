ccc76200154515eb228664792f5d913bb5844e1a
전승원 <78971821+louis7308@users.noreply.github.com>
2022-11-12 23:50:01 +0900

    :fire: :: 안쓰는 import 구문 삭제

--- jpa/src/main/kotlin/com/example/jpa/controller/MemberController.kt
+++ jpa/src/main/kotlin/com/example/jpa/controller/MemberController.kt
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import java.lang.NullPointerException
-import kotlin.reflect.jvm.internal.calls.ThrowingCaller.member
 
 @RestController
 @RequiredArgsConstructor
