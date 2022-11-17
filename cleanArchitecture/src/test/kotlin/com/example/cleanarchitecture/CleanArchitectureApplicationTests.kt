879dee6a8007b901f543e19647e48cc18270cda0
전승원 <78971821+louis7308@users.noreply.github.com>
2022-11-17 22:23:29 +0900

    :sparkles: :: 기본 Test Code

---
+++ cleanArchitecture/src/test/kotlin/com/example/cleanarchitecture/CleanArchitectureApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.cleanarchitecture
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CleanArchitectureApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
