15b511781955fb639a4a43e577b16b719160e162
전승원 <78971821+louis7308@users.noreply.github.com>
2022-11-18 11:44:36 +0900

    :sparkles: :: PostGateway create 추가

---
+++ cleanArchitecture/src/main/kotlin/com/example/cleanarchitecture/infrastructure/gateways/post/PostGateway.kt
@@ -0,0 +1,8 @@
+package com.example.cleanarchitecture.infrastructure.gateways.post
+
+import com.example.cleanarchitecture.infrastructure.gateways.post.model.request.CreatePostGatewayRequestModel
+import com.example.cleanarchitecture.infrastructure.gateways.post.model.response.PostGatewayResponseModel
+
+interface PostGateway {
+    fun create(createPostGatewayRequestModel: CreatePostGatewayRequestModel)
+}
