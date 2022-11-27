c9235b79aa9c59d2322cbd83399c767b77c99628
전승원 <78971821+louis7308@users.noreply.github.com>
2022-11-27 21:14:58 +0900

    :sparkles: :: 기본 main 클린아키텍처

---
+++ cleanArchitecture/src/main/kotlin/com/example/cleanarchitecture/CleanArchitectureApplication.kt
@@ -0,0 +1,11 @@
+package com.example.cleanarchitecture
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CleanArchitectureApplication
+
+fun main(args: Array<String>) {
+    runApplication<CleanArchitectureApplication>(*args)
+}
