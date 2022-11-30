259f64c9c62cc4f8319e87b1539b544de94ede05
전승원 <78971821+louis7308@users.noreply.github.com>
2022-11-30 23:14:48 +0900

    :sparkles: :: h2 의존성 추가

--- hexagonal-architecture/build.gradle.kts
+++ hexagonal-architecture/build.gradle.kts
@@ -32,6 +32,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation ("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly ("com.h2database:h2")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
