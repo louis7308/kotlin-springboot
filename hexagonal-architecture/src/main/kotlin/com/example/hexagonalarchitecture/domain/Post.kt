9f33135e701e38cc0e837bade26b476102a6d5c0
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-05 15:30:19 +0900

    :sparkles: :: Post Domain

---
+++ hexagonal-architecture/src/main/kotlin/com/example/hexagonalarchitecture/domain/Post.kt
@@ -0,0 +1,10 @@
+package com.example.hexagonalarchitecture.domain
+
+class Post(
+    val idx: Int,
+    val title: String,
+    val content: String,
+    val view: Int,
+    val username: String
+) {
+}
