df251bf256d03525af7133f401e7802d5955ac67
전승원 <78971821+louis7308@users.noreply.github.com>
2022-12-05 15:31:09 +0900

    :sparkles: :: 전체 조회 UseCase

---
+++ hexagonal-architecture/src/main/kotlin/com/example/hexagonalarchitecture/application/post/port/in/FindAllPost.kt
@@ -0,0 +1,5 @@
+package com.example.hexagonalarchitecture.application.post.port.`in`
+
+interface FindAllPost {
+    fun findAll() : PostResult
+}
---
+++ hexagonal-architecture/src/main/kotlin/com/example/hexagonalarchitecture/application/post/port/in/PostResult.kt
@@ -0,0 +1,10 @@
+package com.example.hexagonalarchitecture.application.post.port.`in`
+
+class PostResult(
+    val id: Int,
+    val title: String,
+    val content: String,
+    val view: Int,
+    val username: String
+) {
+}
